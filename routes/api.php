2723336b21d53b29b7cf046060c8ca61401dacd1
Herland Mojica Cardozo <mojicacardozoh@gmail.com>
2025-04-13 23:54:16 -0400

    agregando cambios

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// agregando un comentario
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
